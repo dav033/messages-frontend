cfbb10a7ac245d90c84ce05726c9ea9465028742
david theran morales <david.theran030201@gmail.com>
2024-06-14 22:18:23 -0500

    setup the request to server action

--- src/components/home/Rooms.tsx
+++ src/components/home/Rooms.tsx
@@ -1,25 +1,9 @@
 "use server";
 import RoomItem from "./RoomItem";
-import { revalidate } from "@/app/actions";
-
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function getData() {
-  const res = await fetch("http://localhost:8080/rooms", {
-    method: "GET",
-    next: { tags: ["rooms"] },
-  });
-  await sleep(1000);
-
-  revalidate("rooms");
-
-  return res.json();
-}
+import { getRooms } from "@/app/actions";
 
 export default async function Rooms() {
-  const data = await getData();
+  const data = await getRooms();
 
   return (
     <div className="text-white py-4 px-8">
