44b92ca3441892c8250683a96aab9a758fdc3340
david theran morales <david.theran030201@gmail.com>
2024-06-14 22:19:26 -0500

    enable the chats component to join the user in a socket room

--- src/components/sidebar/Chats.tsx
+++ src/components/sidebar/Chats.tsx
@@ -1,14 +1,21 @@
 "use client";
 
+import { useEffect } from "react";
 import ChatItem from "./ChatItem";
+import { socket } from "@/socket";
 
 export default function Chats({ chats }: any) {
-  console.log(chats);
+  useEffect(() => {
+    if (chats) {
+      let rooms = chats.map((chat: any) => chat.id.toString());
 
-  
+      socket.emit("joinRoom", rooms);
+    }
+  }, [chats]);
+
   return (
     <div>
-      {chats.map((chat: any) => {
+      {chats?.map((chat: any) => {
         return <ChatItem key={chat.id} chat={chat} />;
       })}
     </div>
