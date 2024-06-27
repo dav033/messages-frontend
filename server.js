49d9c763a62bcde97baeb9509a81ef6a88a8af8c
david theran morales <david.theran030201@gmail.com>
2024-06-26 21:07:04 -0500

    clean server file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 3000;
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
-let connectedUsers = []; // Array to store connected users
+let connectedUsers = [];
 
 app.prepare().then(() => {
   const httpServer = http.createServer(handler);
@@ -17,43 +17,30 @@ app.prepare().then(() => {
   const io = new Server(httpServer);
 
   io.on("connection", (socket) => {
-    // console.log("a user connected", socket.id);
-
-    // Handle user identification, for example through a login event
     socket.on("login", (userId) => {
       connectedUsers.push({ socketId: socket.id, userId: userId, rooms: [] });
-      // console.log("User logged in: ", userId);
-      // console.log("Connected users: ", connectedUsers);
     });
 
-    // Handle user disconnection
     socket.on("disconnect", () => {
       console.log("a user disconnected", socket.id);
       connectedUsers = connectedUsers.filter(
         (user) => user.socketId !== socket.id
       );
-      // console.log("Connected users: ", connectedUsers);
     });
 
     socket.on("joinRoom", (room) => {
-      // console.log("ERE", room);
       socket.join(room);
-
-      // user.rooms.push(room);
-      // console.log(`User ${user.userId} joined room: ${room}`);
     });
 
     socket.on("message", (data) => {
       const { roomId, message } = data;
 
-      // console.log("AWA", roomId, message);
-
       socket.to(roomId).emit("message", message);
+      socket.to(roomId).emit("messageData", message);
     });
   });
 
   httpServer.once("error", (err) => {
-    // console.error(err);
     process.exit(1);
   });
 
