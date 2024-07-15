c60aa292904aff43dc3467a02bb7356f6e3f82d7
david theran morales <david.theran030201@gmail.com>
2024-07-15 18:52:21 -0500

    Changes input props

--- src/components/FormComponents/Input.tsx
+++ src/components/FormComponents/Input.tsx
@@ -1,14 +1,20 @@
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  accent?: boolean;
 }
 
 export default function Input(props: Props) {
   const { label, className, ...rest } = props;
 
-  const inputClassName = `flex h-10  w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-slate-400 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`;
+  let inputClassName = `flex h-10 mb-16 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-slate-400 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`;
+
+  if (props.accent) {
+    //desabilitar el ring
+    inputClassName += " border-gray-400 focus-visible:ring-0";
+  }
 
   return (
-    <div className="flex flex-col space-y-2 ml-1">
+    <div className="flex flex-col space-y-2 ml-1 mb-2 w-full">
       {label && (
         <label
           className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
